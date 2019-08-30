e186678202d7e525e23aef3438e13f854f1fc6d4
jessio <jzhaha02@126.com>
2019-08-30 19:52:09 +0800

    selenium模拟网页抓取

--- BaikeSpider/spider.py
+++ BaikeSpider/spider.py
@@ -1,4 +1,5 @@
 import json
+import re
 from urllib.parse import urlencode
 import requests
 from requests import RequestException
@@ -6,47 +7,45 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.chrome.options import Options
 from pyquery import PyQuery as pq
 
-browser = webdriver.Chrome('D:\\Program Files (x86)\\Google\\Chrome\\Application\\chromedriver.exe')
-wait = WebDriverWait(browser,10)
+from BaikeSpider.test import main_of_one_person
 
-def get_list(offset, page):
+chrome_options = Options()
+chrome_options.add_argument('--headless')
+chrome_options.add_argument('--disable-gpu')
+browser = webdriver.Chrome('D:\\Program Files (x86)\\Google\\Chrome\\Application\\chromedriver.exe',chrome_options=chrome_options)
+wait = WebDriverWait(browser,10)
+# 获取每页词条
+def get_entry_url(offset, page):
     try:
         data = {
             'limit': 30,
             'index': page,
             'offset': offset
         }
-        url = 'http://baike.baidu.com/fenlei/%E8%89%BA%E6%9C%AF%E5%AE%B6?' + urlencode(data)
+        url = 'https://baike.baidu.com/fenlei/%E8%89%BA%E6%9C%AF%E5%AE%B6?' + urlencode(data)
         browser.get(url)
-        return get_products()
+        return get_url_list()
     except TimeoutError as e:
         print(e)
-        return get_list(offset, page)
+        return get_entry_url(offset, page)
 
-def get_products():
+def get_url_list():
     wait.until(EC.presence_of_all_elements_located((By.CSS_SELECTOR,'#content-main > div.grid.p-container > div.g-row.p-main > div.g71 > div.g-row.p-entry.log-set-param > div.grid-list.grid-list-spot > ul > li:nth-child(1)')))
-    html = browser.page_source
-    doc = pq(html)
-    ul = doc('#content-main > div.grid.p-container > div.g-row.p-main > div.g71 > div.g-row.p-entry.log-set-param > div.grid-list.grid-list-spot > ul').items()
-    for li in ul:
-        product = {
-            'detail': li.find('')
-        }
+    ul = browser.find_element_by_css_selector('#content-main > div.grid.p-container > div.g-row.p-main > div.g71 > div.g-row.p-entry.log-set-param > div.grid-list.grid-list-spot > ul')
+    entries = ul.find_elements_by_css_selector('.photo .pic-content')
+    print(len(entries))
+    for entry in entries:
+        entryurl = entry.get_attribute('href')
+        main_of_one_person(entryurl)
+
+
 
 
 def main():
-    html = get_list(120, 5)
-    print(html)
-    # for url in parse_page_index(html):
-    #     print('url:' + str(url))
-    #     if url:
-    #         detail = get_page_detail(url)
-    #         print(detail)
-
-
-# def parse_page_detail(detail):
+    get_entry_url(120, 5)
 
 
 if __name__ == '__main__':
---
+++ BaikeSpider/test.py
@@ -0,0 +1,69 @@
+import json
+
+import requests
+from requests.exceptions import RequestException
+from pyquery import PyQuery as pq
+
+
+def get_one_page(url):
+    headers = {"User-Agent": "User-Agent:Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; Trident/5.0;"}
+    try:
+        response = requests.get(url, headers=headers)
+        if response.status_code == 200:
+            return response.content.decode('utf8')
+        return None
+    except RequestException as e:
+        return e
+
+
+def parse_one_page(html):
+    doc = pq(html)
+    items_name = list(doc('.basic-info .basicInfo-block .name').items())
+    items_value = list(doc('.basic-info .basicInfo-block .value').items())
+    count = len(items_name)
+    person_dict = {}
+    for i in range(0, count):
+        item_name = items_name[i].text().replace(r'\s+','')
+        item_value = items_value[i].text()
+        if item_name == '中文名':
+            person_dict['user:chineseName'] = item_value
+        elif item_name == '别    名':
+            person_dict['user:additionalName'] = item_value
+        elif item_name == '国    籍':
+            person_dict['user:nationality'] = item_value
+        elif item_name == '民    族':
+            person_dict['user:Nation'] = item_value
+        elif item_name == '出生地':
+            person_dict['user:birthPlace'] = item_value
+        elif item_name == '出生日期':
+            person_dict['user:birthDate'] = item_value
+        elif item_name == '逝世日期':
+            person_dict['user:deathDate'] = item_value
+        elif item_name == '职    业':
+            person_dict['user:Profession'] = item_value
+        elif item_name == '毕业院校':
+            person_dict['user:alumniOf'] = item_value
+        elif item_name == '代表作品':
+            person_dict['user:representativeWorks'] = item_value
+        elif item_name == '主要成就':
+            person_dict['user:achievement'] = item_value
+        elif item_name == '所在剧团':
+            person_dict['user:aﬃliation'] = item_value
+    return person_dict
+
+
+
+
+def main_of_one_person(url):
+    html = get_one_page(url)
+    person_dict = parse_one_page(html)
+    write_to_file(person_dict)
+
+
+
+def write_to_file(content):
+    with open('result.json', 'a', encoding='utf-8') as f:
+        f.write(json.dumps(content, ensure_ascii=False) + '\n')
+        f.close()
+
+
